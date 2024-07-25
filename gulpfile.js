632b3739ccde37bb51957a12d3cb136c64b60c95
Vladislav <vlydikmetal666@mail.ru>
2024-07-25 18:56:29 +0300

    Added modal window and layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,145 +1,138 @@
-import gulp from 'gulp';
-import plumber from 'gulp-plumber';
-import sass from 'gulp-dart-sass';
-import browser from 'browser-sync';
-import postcss from 'gulp-postcss';
-import autoprefixer from 'autoprefixer';
-import csso from 'postcss-csso';
-import rename from 'gulp-rename';
-import svgo from 'gulp-svgmin';
-import svgstore from 'gulp-svgstore';
-import { deleteAsync } from 'del';
-import optimizeImages from 'gulp-optimize-images';
-// import webp from 'gulp-webp';
-import sourcemaps from 'gulp-sourcemaps';
+import gulp from "gulp";
+import plumber from "gulp-plumber";
+import sass from "gulp-dart-sass";
+import browser from "browser-sync";
+import postcss from "gulp-postcss";
+import autoprefixer from "autoprefixer";
+import csso from "postcss-csso";
+import rename from "gulp-rename";
+import svgo from "gulp-svgmin";
+import svgstore from "gulp-svgstore";
+import { deleteAsync } from "del";
+import optimizeImages from "gulp-optimize-images";
+import webp from "gulp-webp";
 
 // Styles
 const styles = () => {
-  return gulp.src('source/sass/style.scss', { sourcemaps: true })
+  return gulp
+    .src("source/sass/style.scss", { sourcemaps: true })
     .pipe(plumber())
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
-    .pipe(browser.stream());
-};
-
-// Minify Styles
-const minifyStyles = () => {
-  return gulp.src('build/css/style.css')
-    .pipe(postcss([
-      csso()
-    ]))
-    .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('build/css'))
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss([autoprefixer(), csso()]))
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css", { sourcemaps: "." }))
     .pipe(browser.stream());
 };
 
 // Copy CSS
 const copyCss = () => {
-  return gulp.src('source/css/*.css') // Assuming reset.css is located in source/css/
-    .pipe(gulp.dest('build/css'));
+  return gulp.src("source/css/*.css").pipe(gulp.dest("build/css"));
 };
 
 // HTML
 const html = () => {
-  return gulp.src('source/*.html')
-    .pipe(gulp.dest('build'));
+  return gulp.src("source/*.html").pipe(gulp.dest("build"));
 };
 
 // Scripts
 const scripts = () => {
-  return gulp.src('source/js/*.js') // Copy all JS files
-    .pipe(gulp.dest('build/js'))
+  return gulp
+    .src("source/js/**/*.js")
+    .pipe(gulp.dest("build/js"))
     .pipe(browser.stream());
 };
 
 // Optimize Images
 const optimizeImagesAll = () => {
-  return gulp.src('source/img/**/*')
-    .pipe(optimizeImages({
-      compressOptions: {
-        jpeg: {
-          quality: 80,
-          progressive: true,
+  return gulp
+    .src("source/img/**/*")
+    .pipe(
+      optimizeImages({
+        compressOptions: {
+          jpeg: {
+            quality: 80,
+            progressive: true,
+          },
+          png: {
+            quality: 90,
+            progressive: true,
+            compressionLevel: 6,
+          },
+          webp: {
+            quality: 80,
+          },
         },
-        png: {
-          quality: 90,
-          progressive: true,
-          compressionLevel: 6,
-        },
-        webp: {
-          quality: 80,
-        },
-      }
-    }))
-    .pipe(gulp.dest('build/img'));
+      })
+    )
+    .pipe(gulp.dest("build/img"));
 };
 
-// // Convert images to WebP
-// const imageWebp = () => {
-//   return gulp.src('source/img/**/*.{png,jpg}')
-//     .pipe(webp())
-//     .pipe(gulp.dest('build/img'));
-// };
+// Convert images to WebP
+const imageWebp = () => {
+  return gulp
+    .src("source/img/**/*.{png,jpg}")
+    .pipe(webp())
+    .pipe(gulp.dest("build/img"));
+};
 
 // Copy Images
 const copyImages = () => {
-  return gulp.src('source/img/**/*.{png,jpg,webp,svg}')
-    .pipe(gulp.dest('build/img'));
+  return gulp
+    .src("source/img/**/*.{png,jpg,webp,svg}")
+    .pipe(gulp.dest("build/img"));
 };
 
 // SVG
 const svg = () => {
-  return gulp.src(['source/img/*.svg', '!source/img/icons/*.svg'])
+  return gulp
+    .src(["source/img/*.svg", "!source/img/icons/*.svg"])
     .pipe(svgo())
-    .pipe(gulp.dest('build/img'));
+    .pipe(gulp.dest("build/img"));
 };
 
 // SVG Sprite
 const sprite = () => {
-  return gulp.src('source/img/icons/*.svg')
+  return gulp
+    .src("source/img/icons/*.svg")
     .pipe(svgo())
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
-    .pipe(rename('sprite.svg'))
-    .pipe(gulp.dest('build/img'));
+    .pipe(
+      svgstore({
+        inlineSvg: true,
+      })
+    )
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
 };
 
 // Copy other files
 const copy = (done) => {
-  gulp.src([
-    'source/fonts/*.{woff2,woff}',
-    'source/*.ico',
-  ], {
-    base: 'source'
-  })
-    .pipe(gulp.dest('build'));
+  gulp
+    .src(["source/fonts/*.{woff2,woff}", "source/*.ico"], {
+      base: "source",
+    })
+    .pipe(gulp.dest("build"));
   done();
 };
 
 // Clean build folder
 const clean = async () => {
-  return await deleteAsync(['build']);
+  return await deleteAsync(["build"]);
 };
 
 // Watcher
 const watcher = () => {
-  gulp.watch('source/sass/**/*.scss', gulp.series(styles, minifyStyles));
-  gulp.watch('source/js/*.js', gulp.series(scripts)); // Watch for changes in all JS files
-  gulp.watch('source/*.html', gulp.series(html, reload));
-  gulp.watch('source/css/*.css', gulp.series(copyCss, reload));
-  gulp.watch('source/img/**/*', gulp.series(copyImages, reload)); // Watch for changes in images
+  gulp.watch("source/sass/**/*.scss", gulp.series(styles));
+  gulp.watch("source/js/*.js", gulp.series(scripts)); // Watch for changes in all JS files
+  gulp.watch("source/*.html", gulp.series(html, reload));
+  gulp.watch("source/css/*.css", gulp.series(copyCss, reload));
+  gulp.watch("source/img/**/*", gulp.series(copyImages, reload)); // Watch for changes in images
 };
 
 // Server
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'build'
+      baseDir: "build",
     },
     cors: true,
     notify: false,
@@ -157,41 +150,14 @@ const reload = (done) => {
 // Build task
 export const build = gulp.series(
   clean,
-  gulp.parallel(
-    copy,
-    copyCss,
-    imageWebp,
-    optimizeImagesAll
-  ),
-  gulp.parallel(
-    styles,
-    html,
-    scripts,
-    svg,
-    sprite,
-  ),
-  minifyStyles
+  gulp.parallel(copy, copyCss, imageWebp, optimizeImagesAll),
+  gulp.parallel(styles, html, scripts, svg, sprite)
 );
 
 // Default task
 export default gulp.series(
   clean,
-  gulp.parallel(
-    copy,
-    copyCss,
-    imageWebp,
-    copyImages,
-  ),
-  gulp.parallel(
-    styles,
-    html,
-    scripts,
-    svg,
-    sprite,
-  ),
-  minifyStyles,
-  gulp.series(
-    server,
-    watcher
-  )
+  gulp.parallel(copy, copyCss, imageWebp, copyImages),
+  gulp.parallel(styles, html, scripts, svg, sprite),
+  gulp.series(server, watcher)
 );
---
+++ source/js/modules/modal.js
@@ -0,0 +1,60 @@
+const modals = () => {
+  function bindModal(triggerSelector, modalSelector, closeSelector) {
+    const trigger = document.querySelectorAll(triggerSelector),
+      modal = document.querySelector(modalSelector),
+      close = document.querySelector(closeSelector),
+      windows = document.querySelectorAll('[data-modal]')
+
+    trigger.forEach((item) => {
+      item.addEventListener('click', (e) => {
+        if (e.target) {
+          e.preventDefault()
+        }
+
+        windows.forEach((item) => {
+          item.style.display = 'none'
+        })
+
+        modal.style.display = 'block'
+        document.body.style.overflow = 'hidden'
+      })
+    })
+
+    close.addEventListener('click', () => {
+      windows.forEach((item) => {
+        item.style.display = 'none'
+      })
+
+      modal.style.display = 'none'
+      document.body.style.overflow = ''
+    })
+
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal) {
+        windows.forEach((item) => {
+          item.style.display = 'none'
+        })
+
+        modal.style.display = 'none'
+        document.body.style.overflow = ''
+      }
+    })
+  }
+
+
+  function showModalByTime(selector, time) {
+    setTimeout(() => {
+      document.querySelector(selector).style.display = 'block'
+      document.body.style.overflow = 'hidden'
+    }, time)
+  }
+
+  bindModal(
+    '.apply-button',
+    '.popup__engineer',
+    '.popup__engineer .popup__close',
+  )
+  showModalByTime(".popup", 30000);
+}
+
+export default modals
